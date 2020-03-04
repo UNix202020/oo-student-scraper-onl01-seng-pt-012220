f2210b1e1d534ca32f7ccab59b48191a2e1a4899
T S <taneymail@gmail.com>
2020-03-04 19:40:21 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,2 +1,26 @@
+require 'open-uri'
+require 'nokogiri'
+require 'pry'
 
+class Scraper
+
+  def self.scrape_index_page(index_url)
+    doc = Nokogiri::HTML(open(index_url))
+    students = []
+    
+    doc.css(".student-card").each do |student|
+      data = {
+        :name => student.css("h4").text,
+        :location => student.css(".student-location").text,
+        :profile_url => student.css("a").attribute("href").value
+      }
+      
+      students << data
+    end
+    students
+  end
+
+  def self.scrape_profile_page(profile_url)
+    doc = Nokogiri::HTML(open(profile_url))
+    student = {}
 
